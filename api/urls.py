8e24be9104a0310428d8f1bfb2552f590ea6e1f0
Tamaz Leladze <taz742b@gmail.com>
2019-04-22 21:02:00 +0400

    added client side. add gitignore and modified docker-compose

--- api/urls.py
+++ api/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('product.urls'), name='product'),
-    path('api/', include('music.urls'), name='product')
 ]
